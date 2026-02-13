6cf187293e2573f4faca1070cb989e29d03b88d5
oluwaremilekun <adesinaremi2019@gmail.com>
2026-02-13 19:30:36 +0000

    Set up Github Actions

--- manual-deploy.sh
+++ manual-deploy.sh
@@ -1,6 +1,6 @@
 ### VARIABLES TO CHANGE - START
-PROJECT_ID=
-SERVICE_NAME=
+PROJECT_ID=oluwaremilekun-adeshina-fisk
+SERVICE_NAME=ise-genai-purple
 ### VARIABLES TO CHANGE - END
 
 # ----------- Manual Deployment ------------ #
