7a44f5b7495d4de984fe5b5a1ed2ebf153fba278
joshua stevenson <jstevenson2024@gmail.com>
2026-02-24 04:39:00 +0000

    returning app.py to intial state

--- app.py
+++ app.py
@@ -5,4 +5,15 @@
 #
 #############################################################################
 
-st.title("App.py")
+import streamlit as st
+
+def display_app_page():
+    """Displays the home page of the app."""
+    st.title('Welcome to SDS!!!!!')
+
+
+
+
+# This is the starting point for your app. You do not need to change these lines
+if __name__ == '__main__':
+    display_app_page()
